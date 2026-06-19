56e8edf93bfd8e983e7fbfe3b0e84eddc5c9e69c
Menaga <priya23011546@gmail.com>
2026-06-19 12:12:39 +0530

    resume

---
+++ HTMLL/post.js
@@ -0,0 +1,42 @@
+// fetch("https://jsonplaceholder.typicode.com/posts")
+// .then(response => response.json())
+// .then(data => {console.log(data)})
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+// .then(response => response.json())
+// .then(data => {console.log(data)})
+// fetch("https://jsonplaceholder.typicode.com/posts/1",
+//     {
+//        method:"DELETE"
+//     }
+// )
+// .then(response => response.json())
+// .then(data => {console.log(data)})
+// fetch("https://jsonplaceholder.typicode.com/posts",{
+//     method: "POST",
+//     headers:{
+//         'Content-Type': 'application/json'
+//     },
+//     body:JSON.stringify({
+//         title:'My post',
+//         body: 'Learning fetch',
+//         userId: 1
+//     })
+    
+// }
+// )
+// .then(response=>response.json())
+// .then(data=>{console.log(data)})
+fetch("https://jsonplaceholder.typicode.com/posts/5",{
+    method: "PATCH",
+    headers:{
+        'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({
+        title:'Title'
+       
+    })
+    
+}
+)
+.then(response=>response.json())
+.then(data=>{console.log(data)})
